99be29b4acee29c840670a6d6ec8b1b4fabcdb5e
Spencer Knight <spencer.knightusn@gmail.com>
2022-05-16 15:16:56 -0700

    removed contact route from misc router

--- routes/miscRouter.js
+++ routes/miscRouter.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const { client, contact, grammar } = require(path.join(
+const { client, grammar } = require(path.join(
   __dirname,
   '../controllers/miscController'
 ));
 
 router.get('/client', client);
-router.post('/contact', contact);
+// router.post('/contact', contact);
 router.post('/grammar', grammar);
 
 module.exports = router;
